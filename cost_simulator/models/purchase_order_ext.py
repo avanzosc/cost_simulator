a26928fef798382c36cf53f261ab6cbca6e7997a
cubells <vicent@vcubells.net>
2014-12-22 13:05:25 +0100

    [BUG] Imposible instalar el módulo con esa condición

--- cost_simulator/models/purchase_order_ext.py
+++ cost_simulator/models/purchase_order_ext.py
@@ -29,10 +29,7 @@ class PurchaseOrder(orm.Model):
         purchase_type_obj = self.pool['purchase.type']
         purchase_type_ids = purchase_type_obj.search(
             cr, uid, [('name', '=', 'Others')], context=context)
-        if not purchase_type_ids:
-            raise orm.except_orm(_('Purchase Type ERROR'),
-                                 _('OTHERS purchase type NOT FOUND'))
-        else:
+        if purchase_type_ids:
             for purchase_type_id in purchase_type_ids:
                 return purchase_type_id
 
@@ -48,7 +45,7 @@ class PurchaseOrder(orm.Model):
         # de compra
         'type_cost': fields.char('Type Cost', size=64),
         # Tipo de compra
-        'type': fields.many2one('purchase.type', 'Type', required=True),
+        'type': fields.many2one('purchase.type', 'Type'),
     }
 
     _defaults = {
